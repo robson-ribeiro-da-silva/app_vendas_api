9782b7ec06efcece1df3691e56769afa2f49c4b7
robson.r.silva <robson.r.silva@kroton.com.br>
2022-12-15 14:10:46 -0300

    Modulo build e deploy

--- src/main/java/com/robson/projeto/vendas/VendasApplication.java
+++ src/main/java/com/robson/projeto/vendas/VendasApplication.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class VendasApplication {
+public class VendasApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
